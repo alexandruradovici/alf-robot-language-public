a5889328d2bb2fd34d083897097a60e7ad6845dc
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2017-03-08 18:48:08 +0200

    jshint and makefile

--- verify/run_all.sh
+++ verify/run_all.sh
@@ -20,56 +20,62 @@ passed=0
 failed=0
 total=0
 
-cd -
+echo '{ "node":true, "esnext":true }' > .jshintrc
+if ! jshint *.js;
+then
+	echo "Please review your code, you have jshint errors"
+else
+	cd -
 
-for folder in robot/*
-do
-	if [ -d $folder ];
-	then
-		if [ -f "$folder"/run.txt ];
+	for folder in robot/*
+	do
+		if [ -d $folder ];
 		then
-			echo `head -n 1 "$folder"/run.txt`
-			P=`head -n 2 "$folder"/run.txt | tail -n 1`
-		else
-			echo `basename $folder`
-			P=10
+			if [ -f "$folder"/run.txt ];
+			then
+				echo `head -n 1 "$folder"/run.txt`
+				P=`head -n 2 "$folder"/run.txt | tail -n 1`
+			else
+				echo `basename $folder`
+				P=10
+			fi
+			if [ $failed == 0 ] || ! (echo $folder | grep bonus &> /dev/null);
+			then
+				for file in "$folder"/*.s
+				do
+					inputfile="verify/$file"
+					outputfile=output/`basename "$file"`.out
+					originalfile="$file.out"
+					errorsfile=output/`basename "$file"`.err
+					title=`head -n 1 "$file" | grep '#' | cut -d '#' -f 2`
+					if [ `echo -n "$title" | wc -c` -eq 0 ];
+					then
+						title=`basename $file`
+					fi
+					node "$1" "$inputfile" > "$outputfile"
+					strtitle="Verifying $title"
+					printf '%s' "$strtitle"
+					pad=$(printf '%0.1s' "."{1..60})
+					padlength=65
+					if diff "$originalfile" "$outputfile" &> "$errorsfile"
+					then
+						str="ok (""$P""p)"
+						passed=$(($passed+1))
+						POINTS=$(($POINTS+$P))
+					else
+						str="error (0p)"
+						failed=$(($failed+1))
+					fi
+					total=$(($total+1))
+					printf '%*.*s' 0 $((padlength - ${#strtitle} - ${#str} )) "$pad"
+				    printf '%s\n' "$str"
+				done
+			else
+				echo "Not verifying bonus, you have $failed failed tests"
+			fi
 		fi
-		if [ $failed == 0 ] || ! (echo $folder | grep bonus &> /dev/null);
-		then
-			for file in "$folder"/*.s
-			do
-				inputfile="verify/$file"
-				outputfile=output/`basename "$file"`.out
-				originalfile="$file.out"
-				errorsfile=output/`basename "$file"`.err
-				title=`head -n 1 "$file" | grep '#' | cut -d '#' -f 2`
-				if [ `echo -n "$title" | wc -c` -eq 0 ];
-				then
-					title=`basename $file`
-				fi
-				node "$1" "$inputfile" > "$outputfile"
-				strtitle="Verifying $title"
-				printf '%s' "$strtitle"
-				pad=$(printf '%0.1s' "."{1..60})
-				padlength=65
-				if diff "$originalfile" "$outputfile" &> "$errorsfile"
-				then
-					str="ok (""$P""p)"
-					passed=$(($passed+1))
-					POINTS=$(($POINTS+$P))
-				else
-					str="error (0p)"
-					failed=$(($failed+1))
-				fi
-				total=$(($total+1))
-				printf '%*.*s' 0 $((padlength - ${#strtitle} - ${#str} )) "$pad"
-			    printf '%s\n' "$str"
-			done
-		else
-			echo "Not verifying bonus, you have $failed failed tests"
-		fi
-	fi
-done
+	done	
+fi
 
 echo 'Tests: ' $passed '/' $total
 echo 'Points: '$POINTS
