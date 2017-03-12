95d299f46093d33e40968d3f92e8be4b081e2126
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2017-03-12 12:27:12 +0200

    refine error list

--- verify/run_all.sh
+++ verify/run_all.sh
@@ -91,6 +91,6 @@ echo 'Points: '$POINTS
 
 if [ "$passed" != "$total" ];
 then
-	echo -e "Original File \t| Your file"
+	echo -e "Original File                            | Your file"
 	cat $errorslist 1>&2
 fi
